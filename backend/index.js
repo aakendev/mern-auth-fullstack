959a6c91c3761b1659a9e92d25b1c698eeb32a8b
Aldy Rizky Putra <aakendev@gmail.com>
2023-10-14 17:13:49 +0000

    Add a Middleware and Error Handling

--- backend/index.js
+++ backend/index.js
@@ -30,3 +30,13 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
